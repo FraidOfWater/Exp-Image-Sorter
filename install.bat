e7dc9a7277b236ccb441b4aad9eb16ab6c4dccc9
FraidOfWater <141600613+FraidOfWater@users.noreply.github.com>
2025-01-03 10:28:43 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
-pip install pillow pyvips imageio imageio[ffmpeg] python-vlc tkinter-tooltip psutil
+pip install pillow pyvips imageio imageio[ffmpeg] python-vlc tkinter-tooltip psutil pyinstaller
 pause
