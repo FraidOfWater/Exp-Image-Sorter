955ba7a2272020ad82ceda9c79f0ac7a9839a0e7
FraidOfWater <141600613+FraidOfWater@users.noreply.github.com>
2025-01-12 07:32:40 +0200

    before overhaul

--- destination_viewer.py
+++ destination_viewer.py
@@ -1,8 +1,7 @@
 import os
+from time import perf_counter, sleep
 
-from time import perf_counter
-
-from threading import Thread
+from threading import Thread, Event
 from concurrent.futures import ThreadPoolExecutor
 
 from gc import collect
@@ -44,6 +43,8 @@ class Destination_Viewer():
         self.gui = fileManager.gui
         style = ttk.Style()
         style.configure("Theme_square.TCheckbutton", background=self.gui.grid_background_colour, foreground=self.gui.text_colour)
+        self.displayedlist = []
+        self.window_is_closing = False
 
     def get_paths(self): # needed? ###
         "Create a list for every buttondest" "If loadsession=True, sort everything from assigned and moved to the lists"
@@ -55,12 +56,14 @@ class Destination_Viewer():
 
     def create_window(self, *args):
         if hasattr(self, 'destwindow'):
-            self.close_window()
+            self.close_window(reopenargs=args, reopen=True)
+            return
+        self.window_is_closing = False
         button_info = args[1]
         self.dest_path = button_info['path']
         self.dest_color = button_info['color']
         self.displayedlist = []
-        self.displayedlist_ids = []
+        self.displayedlist_ids = [] #make set?
         self.displayedset = set()
         self.animated = []
         self.destwindow = tk.Toplevel()
@@ -83,47 +86,60 @@ class Destination_Viewer():
         # Check all in assigned and moved lists. If the path matches (assigned), and for moved, path matches, makedestsquare.
         create_list = []
         for x in self.gui.gridmanager.gridsquarelist:
-            if x.obj.dest:
-                create_list.append(x.obj)
+            if x.obj.dest == self.dest_path:
+                create_list.append((x.obj, "add"))
             elif x.obj.moved and x.obj.path == dest:
-                create_list.append(x.obj)
+                create_list.append((x.obj, "add"))
         self.add_squares(create_list)
 
-    def add_squares(self, imageobjects: list) -> None:
+    def add_squares(self, imagetuples: list) -> None:
         "Adds squares to grid, displayedlist, and reloads them"
-        # we get obj make square and add it.
+        # we get obj make square and add it. # 0 is obj, 1 is action
         new = []
         regen = []
-        for obj in imageobjects:
-            if obj.dest == self.dest_path:
-                new.append(self.makedestsquare(obj))
+        for obj in imagetuples:
+            if obj[1] == "add":
+                new.append((self.makedestsquare(obj[0]), "add"))
+            elif obj[1] == "refresh":
+                new.append((obj[0], "refresh")) # add destsquare
+
         for gridsquare in new:
-            self.destgrid.window_create(
-                "insert", window=gridsquare, padx=self.gui.gridsquare_padx, pady=self.gui.gridsquare_pady)
-            if gridsquare.obj.guidata['destimg'] == None: # Checks if img is unloaded
-                regen.append(gridsquare)
-            self.displayedlist.append(gridsquare)
-            self.displayedlist_ids.append(gridsquare.obj.id)
-            self.displayedset.add(gridsquare)
+            if gridsquare[1] == "add":
+                self.destgrid.window_create(
+                "1.0", window=gridsquare[0], padx=self.gui.gridsquare_padx, pady=self.gui.gridsquare_pady)
+                self.displayedlist.append(gridsquare[0])
+                self.displayedlist_ids.append(gridsquare[0].obj.id)
+                self.displayedset.add(gridsquare[0])
+                
+            elif gridsquare[1] == "refresh":
+                self.destgrid.window_configure(gridsquare[0], window="")
+                self.displayedlist.remove(gridsquare[0])
+                self.displayedlist_ids.remove(gridsquare[0].obj.id)
+                self.destgrid.window_create(
+                "1.0", window=gridsquare[0], padx=self.gui.gridsquare_padx, pady=self.gui.gridsquare_pady)
+                self.displayedlist.append(gridsquare[0])
+                self.displayedlist_ids.append(gridsquare[0].obj.id)
+            
+            if gridsquare[0].obj.guidata['destimg'] == None: # Checks if img is unloaded
+                regen.append(gridsquare[0])
+            
         if regen:
             self.dest_thumbs.reload(regen) # Threads the reloading process. #need private attribute for dest and grid separate.
 
-    def remove_squares(self, squares: list) -> None:
+    def remove_squares(self, squares: list, unload) -> None:
         "Removes square from grid, displayedlist, and can unload it from memory"
-        remove = []
-        # DETECTED CHANGE, BUT OBJ ID ALREADY IN LIST. THIS MEANS REMOVE IF WIDNOW DEST AND OBJ DEST NOT SAME. (OBJ DEST CHANGED)
-        for square in squares:
-            if self.dest_path != square.obj.dest: #old vs new dest
-                a = self.displayedlist_ids.index(square.obj.id)
-                remove.append(self.displayedlist[a])
-
-        for gridsquare in remove:
+    
+        unload_list = []
+        for gridsquare in squares:
             self.destgrid.window_configure(gridsquare, window="")
             self.displayedlist.remove(gridsquare)
             self.displayedlist_ids.remove(gridsquare.obj.id)
             self.displayedset.discard(gridsquare)
-        self.dest_thumbs.unload(remove)
-        print("Unloading:", len(remove))
+            if unload:
+                unload_list.append(gridsquare)
+        if unload:
+            self.dest_thumbs.unload(unload_list)
+            print("Unloading:", len(unload_list))
 
     def makedestsquare(self, imageobj):
 
@@ -164,7 +180,7 @@ class Destination_Viewer():
             frame.cf = check_frame
 
             #Create different dest for destinations to control view better. These also call a command to cancel the viewer image from being moved by keypresses, if we interact with other gridsquares first.
-            check = ttk.Checkbutton(check_frame, textvariable=truncated_name_var, variable=imageobj.checked, onvalue=True, offvalue=False, command=lambda: (self.gui.uncheck_show_next()), style="Theme_square.TCheckbutton")
+            check = ttk.Checkbutton(check_frame, textvariable=truncated_name_var, variable=imageobj.checked, onvalue=True, offvalue=False, command=lambda: (setattr(self.gui, 'focused_on_secondwindow', False)), style="Theme_square.TCheckbutton")
             check.grid(sticky="NSEW")
 #
             ## save the data to the image obj to both store a reference and for later manipulation
@@ -190,7 +206,7 @@ class Destination_Viewer():
             print("Makedestsquare error:", e)
         return frame
 
-    def close_window(self, *args, event=False):
+    def close_window(self, *args, event=False, reopenargs=None, reopen=False): # must thread this because of unload is threaded.
         "Safely close everything"
         if event:
             event = args[0] # reappoint event variable to mouseEvent
@@ -199,14 +215,21 @@ class Destination_Viewer():
                    square.winfo_y() <= event.y <= square.winfo_y() + square.winfo_height():
                     print("Click inside a square, not closing.")
                     return  # Click is inside a square, do not close
+        self.window_is_closing = True
         self.dest_thumbs.unload(self.displayedlist)
+
         if self.fileManager.navigator.old in self.displayedset:
             self.fileManager.navigator.old = None
+            
         for x in self.displayedlist:
             x.canvas.destroy()
             x.destroy()
             del x.canvas
             del x
+        del self.displayedlist_ids
+        del self.animated
+        del self.displayedlist
+        del self.displayedset
         if hasattr(self, "destgrid"):
             self.destgrid.destroy()
             del self.destgrid
@@ -214,27 +237,45 @@ class Destination_Viewer():
             self.gui.destpane_geometry = self.destwindow.winfo_geometry()
             self.destwindow.destroy()
             del self.destwindow
-        del self.displayedlist_ids
-        del self.animated
-        del self.displayedlist
-        del self.displayedset
         collect()
 
+        if reopen == True:
+            a, b = reopenargs
+            self.create_window(a, b)
+            self.fileManager.navigator.window_focused = "DEST"
+            pass
+        else:
+            self.fileManager.navigator.window_focused = "GRID"
+            self.fileManager.navigator.displayedlist = self.gui.gridmanager.displayedlist
+        print("deleted")
+        
+# could have shared frames, if open elsewhere, dont unload. very easy. one reload call to sortimages. not this duplicate stuff.
 class ThumbManager:
     def __init__(self, dest_viewer):
+        self.dest_viewer = dest_viewer
         self.animate_dest = dest_viewer.animate_dest
         self.threads = dest_viewer.fileManager.threads
         self.data_dir = dest_viewer.fileManager.data_dir
         self.gui = dest_viewer.fileManager.gui
-        self.thread = None
-        self.gen_thread = None
+        self.fileManager = dest_viewer.fileManager
 
+        self.gen_thread = None
+        self.running_thread = None
+        self.new_thread = None
+
+        self.gen_queue = []
+        self.queue = []
+    
     def reload(self, gridsquares):
         #queue system. executor global, submit/map to it def concurrent(squares, workers) 1 persistent thread for it. no daemon
-        def multithread():
-            self.thread = new_thread
+        self.this_thread = None
+        def multithread1():
+            if self.gen_thread and self.gen_thread.is_alive(): self.gen_thread.join()
+            if self.running_thread and self.running_thread.is_alive(): self.running_thread.join()
+            self.running_thread = self.new_thread
+            self.this_thread = self.running_thread
             a = perf_counter()
-            animated = [x for x in gridsquares if x.obj.isanimated and x.obj.framecount != 0]
+            animated = [x for x in gridsquares if x.obj.framecount > 1]
             try:
                 max_workers = max(1,self.threads*2)
                 with ThreadPoolExecutor(max_workers=max_workers) as executor:
@@ -244,16 +285,36 @@ class ThumbManager:
                 with ThreadPoolExecutor(max_workers=max_workers) as executor:
                     executor.map(reload_animated, animated)
                 print(f"Thumbframes loaded in: {perf_counter()-a:.2f}")
+                #if self.queue:
+                #    queue_emptier()
             except Exception as e:
                 print("Error reloading thumbs and frames", e)
+
+        def queue_emptier():
+            with ThreadPoolExecutor(max_workers=max(1,self.threads)) as executor:
+                while self.queue and (not self.fileManager.program_is_exiting and not self.dest_viewer.window_is_closing):
+                    gridsquare = self.queue[0]
+                    frames_loaded = 0
+                    if hasattr(self.animate_dest, "running"):
+                        test = list(self.fileManager.animate.running) + list(self.animate_dest.running)
+                    else: test = list(self.fileManager.animate.running)
+                    for x in test:
+                        frames_loaded += len(x.obj.frametimes)
+                    # Check if we can load more frames
+                    if frames_loaded + len(gridsquare.obj.frametimes) < self.fileManager.max_concurrent_frames:
+                        self.queue.pop(0)
+                        future = executor.submit(reload_animated, gridsquare)
+                    sleep(0.3)  # Sleep to avoid busy waiting
         def reload_static(gridsquare):
+            if self.fileManager.program_is_exiting:
+                return
             imageobj = gridsquare.obj
             img = None
             try:
                 try:
 
                     #this is faster
-                    buffer = pyvips.Image.new_from_file(imageobj.thumbnail)
+                    buffer = pyvips.Image.new_from_file(imageobj.thumbnail, access="sequential")
                     img = ImageTk.PhotoImage(Image.frombuffer(
                         "RGB", [buffer.width, buffer.height], buffer.write_to_memory()))
                     
@@ -266,41 +327,59 @@ class ThumbManager:
             except Exception as e:
                 print(f"Error in load_thumb: {e}")
         def reload_animated(gridsquare):
+            if self.fileManager.program_is_exiting or self.dest_viewer.window_is_closing: return
+            if self.this_thread != self.new_thread: return
             obj = gridsquare.obj
+            if obj.framecount == 2 and round(obj.file_size/1.048576/1000000,2) > 10:
+                print(f"{obj.name.get()} is too big for grid animation.")
+                return
+            if obj.path.lower().endswith(".mp4"):
+                return
             #gridsquare in self.animate.running
             if len(obj.frames_dest) == obj.framecount: # we switched to a view where the gif is already playing, skip
                 return
             if len(obj.frames_dest) != obj.framecount and len(obj.frames_dest) > 0: #another thread is active
                 return
+            frames_loaded = 0
+            if hasattr(self.animate_dest, "running"):
+                test = list(self.fileManager.animate.running) + list(self.animate_dest.running)
+            else: test = list(self.fileManager.animate.running)
+            for x in test: ### account for dest running.
+                frames_loaded += len(x.obj.frametimes)
+            if not frames_loaded + len(obj.frametimes) < self.fileManager.max_concurrent_frames:
+                if gridsquare not in self.queue:
+                    self.queue.append(gridsquare)
+                return
+            
             obj.frames_dest = [] #make sure it is empty for sure
+            obj.lazy_loading_dest = True
+
             if obj.path.lower().endswith(".webm"):
                 reader = None
                 try:
                     reader = get_reader(obj.path)
-                    fps = (reader.get_meta_data().get('fps', 24))
-                    obj.delay = int(round((1 / fps)*1000))
                     for frame in reader:
+                        if self.fileManager.program_is_exiting: return
+                        if self.this_thread != self.new_thread: return
                         image = Image.fromarray(frame)
                         image.thumbnail((self.gui.thumbnailsize,self.gui.thumbnailsize))
                         tk_image = ImageTk.PhotoImage(image)
                         obj.frames_dest.append(tk_image)
                         self.animate_dest.add_animation(obj)
 
-                    obj.lazy_loading = False
+                    obj.lazy_loading_dest = False
                 except Exception as e:
                     print(f"Error in frame generation for grid: {e}")
                 finally:
-                    reader.close()
-                return
-            elif obj.path.lower().endswith(".mp4"):
-                return
+                    if reader:
+                        reader.close()
             # Load frames for GIF, WEBP
             else:
                 try:
                     with Image.open(obj.path) as img:
-                        if obj.framecount == 0: # Static
-                            return
                         for i in range(obj.framecount):
+                            if self.fileManager.program_is_exiting: return
+                            if self.this_thread != self.new_thread: return
                             img.seek(i)  # Move to the ith frame
                             frame = img.copy()
                             frame.thumbnail((self.gui.thumbnailsize, self.gui.thumbnailsize), Image.Resampling.LANCZOS)
@@ -308,33 +387,32 @@ class ThumbManager:
                             obj.frames_dest.append(tk_image)
                             self.animate_dest.add_animation(obj)
 
-                        obj.lazy_loading = False
+                        obj.lazy_loading_dest = False
                         print(f"All frames loaded for: {obj.name.get()[:30]}")
                 except Exception as e: #fallback to static.
                     print(f"Error in reload_thumbframes (): {e}")
-                    obj.isanimated = False
-        if gridsquares:
-            new_thread = Thread(target=multithread, daemon=True)
-            new_thread.start()
-            print("(Thread) Reloading:", len(gridsquares))
-
-    def unload(self, gridsquares): # thread?
+        if not gridsquares: return
+        #if (self.running_thread and self.running_thread.is_alive()): return
+        self.new_thread = Thread(target=multithread1, daemon=True)
+        self.new_thread.start()
+        print("(Thread) Reloading:", len(gridsquares))
+    def unload(self, gridsquares): # thread? ### need thread because unload is very slow for 1000 pics.
         def unload_static(i):
-            i.canvas.itemconfig(i.canvas_image_id, image=None)
+            i.canvas.itemconfig(i.canvas_image_id, image=None) # would destroying the canvas clear these? should maybe test.
             i.canvas.image = None
             i.obj.guidata["destimg"] = None
         def unload_animated(i):
             self.animate_dest.remove_animation(i, self.gui.square_colour)
             i.obj.index_dest = 0
             i.obj.frames_dest = []
-            i.obj.lazy_loading = True
+            i.obj.lazy_loading_dest = True
         for gridsquare in gridsquares:
             if gridsquare.obj.frames_dest:
                 unload_animated(gridsquare)
             unload_static(gridsquare)
+        collect()
         #collect() # gc takes long. maybe we can tell it what to do? why is gc necessary even? something STILL pointing to the image? ###
         # see if we can have tkinter reference the images from imagefile context.
-    #setdest, moveall to thumbmanager? walk here?
 
 class Animate:
     def __init__(self):
@@ -360,10 +438,10 @@ class Animate:
             if not i.obj.frames_dest: # No frames have been initialized. Shouldn't happen ever. Dead code?
                 print("Error, lazy called with no frames")
                 return
-            if not i.obj.lazy_loading and len(i.obj.frames_dest) != i.obj.framecount: # All frames generated doesnt match expected (only webm, dead?)
+            if not i.obj.lazy_loading_dest and len(i.obj.frames_dest) != i.obj.framecount: # All frames generated doesnt match expected (only webm, dead?)
                 print("Error, frames generated doesnt match expected")
                 return
-            if not i.obj.lazy_loading and len(i.obj.frames_dest) == i.obj.framecount: # All frames ready. (second part only webm, dead)
+            if not i.obj.lazy_loading_dest and len(i.obj.frames_dest) == i.obj.framecount: # All frames ready. (second part only webm, dead)
                 i.obj.guidata["canvas"]['background'] = "green" # move to laZY?
                 loop(i)
             else:
@@ -387,6 +465,3 @@ class Animate:
                 i.obj.index_dest = (i.obj.index_dest + 1) % i.obj.framecount
                 i.canvas.after(i.obj.frametimes[i.obj.index_dest], lambda: loop(i)) #run again.
         lazy(gridsquare) # Non threaded
-    def stop_animations(self, gridsquare):
-        self.running.clear()
-        self.thread = None
