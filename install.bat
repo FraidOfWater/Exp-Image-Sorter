6a914dbc7d2407b76f09c0d91a273b1a2a2f8953
FraidOfWater <141600613+FraidOfWater@users.noreply.github.com>
2025-03-21 12:01:59 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
-pip install pillow pyvips imageio imageio[ffmpeg] numpy python-vlc psutil pyinstaller
+pip install pillow pyvips imageio imageio[ffmpeg] numpy python-vlc psutil pyinstaller colorthief scikit-learn
 pause
